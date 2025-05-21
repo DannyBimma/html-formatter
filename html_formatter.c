00ef9a4fde21152b3135fa15bf512d04d63f9e20
DannyBimma <dantrotman92@gmail.com>
2025-05-20 21:53:51 -0400

    Add file opening

--- html_formatter.c
+++ html_formatter.c
@@ -13,4 +13,12 @@ int main(int argc, char *argv[])
         printf("Usage: %s <filename>\n", argv[0]);
         return 1;
     }
+
+    // Open input
+    input_file = fopen(argv[1], "r");
+    if (!input_file)
+    {
+        fprintf(stderr, "Error: Could not open file.\n Please check path or file name! %s\n", argv[1]);
+        return 1;
+    }
 }
