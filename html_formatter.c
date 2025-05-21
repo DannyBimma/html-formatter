4af7df4f0fdb3d2eb04d223065020d96077b8894
DannyBimma <dantrotman92@gmail.com>
2025-05-20 22:02:34 -0400

    Create and name output files

--- html_formatter.c
+++ html_formatter.c
@@ -19,6 +19,19 @@ int main(int argc, char *argv[])
     if (!input_file)
     {
         fprintf(stderr, "Error: Could not open file.\n Please check path or file name! %s\n", argv[1]);
-        return 1;
+        return 2;
+    }
+
+    // Write output file name
+    strcpy(output_filename, argv[1]);
+    strcat(output_filename, ".html");
+
+    // Open output
+    output_file = fopen(output_filename, "w");
+    if (!output_file)
+    {
+        fprintf(stderr, "Error: Could not create output file.\n Please check file permissions! %s\n", output_filename);
+        fclose(input_file);
+        return 3;
     }
 }
