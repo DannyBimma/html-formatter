ae758fb16a94fca60b8983c0d0316a370bad1c0a
DannyBimma <dantrotman92@gmail.com>
2025-05-20 21:47:26 -0400

    Add FILE pointers

--- html_formatter.c
+++ html_formatter.c
@@ -4,6 +4,10 @@
 
 int main(int argc, char *argv[])
 {
+    FILE *input_file;
+    FILE *output_file;
+    char output_filename[256];
+
     if (argc != 2)
     {
         printf("Usage: %s <filename>\n", argv[0]);
